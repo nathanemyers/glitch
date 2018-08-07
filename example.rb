22a49ff95f4cd9df4d3d391616f5286e650a7da9
Nathan Myers <nathanemyers@gmail.com>
2018-08-06 22:21:41 -0500

    adding converter script

---
+++ example.rb
@@ -0,0 +1,60 @@
+require 'pnglitch'
+
+count = 0
+infiles = %w(lena.png lena-alpha.png)
+infiles.each do |file|
+  alpha = /alpha/ =~ file
+  [false, true].each do |compress|
+    [false, true].each do |interlace|
+      infile = file
+      if interlace
+        system("convert -interlace plane %s tmp.png" % infile)
+        infile = 'tmp.png'
+      end
+      [:optimized, :sub, :up, :average, :paeth].each do |filter|
+        [:replace, :transpose, :defect].each do |method|
+          count += 1
+          png = PNGlitch.open infile
+          png.change_all_filters filter unless filter == :optimized
+          options = [filter.to_s]
+          options << 'alpha' if alpha
+          options << 'interlace' if interlace
+          options << 'compress' if compress
+          options << method.to_s
+          outfile = "lena-%03d-%s.png" % [count, options.join('-')]
+
+          process = lambda do |data, range|
+            case method
+            when :replace
+              range.times do
+                data[rand(data.size)] = 'x'
+              end
+              data
+            when :transpose
+              x = data.size / 4
+              data[0, x] + data[x * 2, x] + data[x * 1, x] + data[x * 3..-1]
+            when :defect
+              (range / 5).times do
+                data[rand(data.size)] = ''
+              end
+              data
+            end
+          end
+
+          unless compress
+            png.glitch do |data|
+              process.call data, 50
+            end
+          else
+            png.glitch_after_compress do |data|
+              process.call data, 10
+            end
+          end
+          png.save outfile
+          png.close
+        end
+      end
+    end
+  end
+end
+File.unlink 'tmp.png'
--- glitch.rb
+++ glitch.rb
@@ -1,15 +1,12 @@
 require 'pnglitch'
 require 'random-word'
 
-infile = 'source_images/'
-outfile = 'test.png'
-
 def select_file
   files = Dir["source_images/*.png"]
   return files[rand(files.length)]
 end
 
-def wrong_filter scanline
+def wrong_filter(scanline)
   scanline.register_filter_encoder do |data, prev|
     data.size.times.reverse_each do |i|
       x = data.getbyte(i)
@@ -20,25 +17,25 @@ def wrong_filter scanline
   end
 end
 
-def write_random_data scanline
+def write_random_data(scanline)
   byte = Random.new.bytes(1)
   data = scanline.data
   scanline.replace_data(data.gsub(/\d/, byte))
 end
 
-def light_corrupt data
+def light_corrupt(data)
   #byte = Random.new.bytes(1)
   byte = 'x'
   data.gsub(/\d/, byte )
 end
 
-def heavy_corrupt data
+def heavy_corrupt(data)
   #byte = Random.new.bytes(1)
   byte = 'x'
   data.gsub(/\w/, byte )
 end
 
-def glitch_layer png, num, size
+def glitch_layer(png, num, size)
   puts "glitching layer #{num}, size #{size}"
   png.each_scanline do |scanline|
     scanline.change_filter num 
@@ -47,6 +44,58 @@ def glitch_layer png, num, size
   end
 end
 
+def garble(png, chance)
+  stash = []
+  buf = 2 ** 18
+  png.glitch_as_io do |io|
+    until io.eof? do
+      d = io.read(buf)
+      puts "got #{d.size} amount of data"
+      io.pos -= d.size
+
+      if rand(chance) == 0
+        puts "stashing"
+        if not stash.empty?
+          puts "popping"
+          io.print(stash.pop)
+        else
+          stash.push(d)
+        end
+      else
+        io.print(d)
+      end
+
+    end
+  end
+end
+
+def all_filter_two(png)
+  p.change_all_filters 2
+  p.each_scanline do |l|
+    l.register_filter_encoder do |data, prev|
+      data.size.times.reverse_each do |i|
+        x = data.getbyte(i)
+        v = prev ? prev.getbyte(i) : 0
+        data.setbyte(i, (x - v) & 0xfe)
+      end
+      data
+    end
+  end
+end
+
+# file management
+if ARGV[0]
+  file = ARGV[0]
+else
+  file = select_file
+end
+
+
+puts "selecting #{file}"
+filename = File.basename(file)
+adj = RandomWord.adjs.next
+outfile = "output/#{adj}-#{filename}"
+
 # 0 - blocky
 # 1 - horizontal lines
 # 2 - vertical lines
@@ -54,16 +103,18 @@ end
 # 4 - peath
 begin
   `say "glitching..."`
-  file = select_file
-  puts "selecting #{file}"
   PNGlitch.open(file) do |png|
-    iterations = rand(3) + 1
-    puts "using #{iterations} iterations"
-    (1..iterations).each do |n|
-      layer = rand(5)
-      size = 1
-      glitch_layer png, layer, size
-    end
+    #iterations = rand(3) + 1
+    #puts "using #{iterations} iterations"
+    #(1..iterations).each do |n|
+      #layer = rand(5)
+      #size = 1
+      #glitch_layer png, layer, size
+    #end
+    
+    all_filter_two(png)
+
+    #garble(png, 10)
 
     #puts "glitch"
     #png.glitch do |data|
@@ -71,11 +122,8 @@ begin
     #end
 
     # save this bad boy
-    filename = File.basename(file)
-    adj = RandomWord.adjs.next
-    outfile = "output/#{adj}-#{filename}"
     puts "saving to #{outfile}"
-    png.save outfile
+    png.save(outfile)
 
     `say "glitching complete"`
   end
