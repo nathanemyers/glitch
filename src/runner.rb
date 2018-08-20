0a12e7c54d772f23d25e87294bb0c0582df8e5e6
Nathan Myers <nathanemyers@gmail.com>
2018-08-19 22:59:26 -0500

    adding menu structure

--- src/runner.rb
+++ src/runner.rb
@@ -1,7 +1,51 @@
 #!/usr/bin/env ruby
 
-require_relative 'profile'
+require_relative 'instruction'
 
-profile = Glitch::Instruction.new(1, 1)
 
-puts profile.layer
+def create_random_profile
+  num_instructions = rand(3) + 1
+
+  instructions = []
+
+  while num_instructions > 0
+    num_instructions = num_instructions - 1 
+      instructions.push(Glitch::Instruction.random_instruction)
+  end
+
+  return instructions
+end
+
+
+def welcome
+  puts ""
+  puts "🤠 Welcome to Glitch Town!"
+  puts ""
+end
+
+def main_menu
+  puts "Menu Items:"
+  puts " [C]reate Random Glitch"
+  puts " [Q]uit"
+end
+
+welcome
+
+loop do
+  main_menu
+  input = gets.chomp
+  puts ""
+
+  case input.downcase
+  when "c"
+    puts "🤪  Glitching..."
+    puts create_random_profile
+  when "q"
+    puts "😢 Goodbye"
+    break
+  else
+    puts "😩 I don't know what that command is!"
+  end
+
+  puts ""
+end
