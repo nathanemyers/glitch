07b50411df43b6eda16802ebf21ab2dacc8ae60c
Nathan Myers <nathanemyers@mgmail.com>
2020-02-16 16:39:40 -0600

    creating db schema

--- server/db.rb
+++ server/db.rb
@@ -15,18 +15,48 @@ class Database
 
   end
 
-
-
   private
 
   def init(db) 
     db = SQLite3::Database.new db
 
-    # Create a table
+    # Create Source Image Table
     db.execute <<-SQL
-      create table numbers (
-        name varchar(30),
-        val int
+      CREATE TABLE source_images (
+        id  INT  PRIMARY KEY  NOT NULL,
+        URL TEXT              NOT NULL 
+      );
+    SQL
+
+    # Create Glitch Image Table
+    db.execute <<-SQL
+      CREATE TABLE glitch_images (
+        id                  INT   PRIMARY KEY  NOT NULL,
+        URL                 TEXT               NOT NULL,
+        instruction_set_id  INT,
+        source_image_id     INT,
+        run_id              INT,
+        FOREIGN KEY(instruction_set_id) REFERENCES instruction_set(id),
+        FOREIGN KEY(source_image_id)    REFERENCES source_images(id)
+        FOREIGN KEY(run_id)             REFERENCES runs(id)
+      );
+    SQL
+
+    # Create Instruction Set Table
+    db.execute <<-SQL
+      CREATE TABLE instruction_set (
+        id           INT   PRIMARY KEY  NOT NULL,
+        name         TEXT               NOT NULL,
+        instructions BLOB               NOT NULL
+      );
+    SQL
+
+
+    # Create Runs Table
+    db.execute <<-SQL
+      CREATE TABLE runs (
+        id           INT   PRIMARY KEY  NOT NULL,
+        name         TEXT               NOT NULL,
       );
     SQL
 
