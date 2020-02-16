2505c246ee205ab914c87ad7110b91b14f7e1180
Nathan Myers <nathanemyers@mgmail.com>
2020-02-16 16:11:24 -0600

    basic working db!

--- server/app.rb
+++ server/app.rb
@@ -1,4 +1,11 @@
+#!/usr/bin/env ruby
+#
 require 'sinatra'
+require_relative 'db'
+
+DB_NAME = "glitch.db"
+
+db = Database.new(DB_NAME)
 
 get '/random_glitch' do
 
--- server/db.rb
+++ server/db.rb
@@ -1,11 +1,37 @@
 require "sqlite3"
 
-db = SQLite3::Database.new "test.db"
+class Database
 
-# Create a table
-rows = db.execute <<-SQL
-  create table numbers (
-    name varchar(30),
-    val int
-  );
-SQL
+  def initialize(db)
+    puts "DB Bootup..."
+    begin
+      @db = SQLite3::Database.open db
+    rescue
+      puts "Could not find database: #{db}"
+      puts "Initializing new database: #{db}..."
+      @db = init(db)
+      puts "Done!"
+    end
+
+  end
+
+
+
+  private
+
+  def init(db) 
+    db = SQLite3::Database.new db
+
+    # Create a table
+    db.execute <<-SQL
+      create table numbers (
+        name varchar(30),
+        val int
+      );
+    SQL
+
+    return db
+  end
+
+
+end
