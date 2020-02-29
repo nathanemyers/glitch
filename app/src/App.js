791c3457cffb96fdbc0ac486ff1052934f3ca7ab
Nathan Myers <nathanemyers@mgmail.com>
2020-02-29 16:44:14 -0600

    adding font

--- app/src/App.js
+++ app/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import styled from 'styled-components'
 import {
   BrowserRouter as Router,
   Switch,
@@ -12,31 +13,37 @@ import NavItem from './components/NavItem'
 import Home from './containers/Home'
 import Runs from './containers/Runs'
 
+const Container = styled.div`
+  font-family: 'Nunito Sans';
+`
+
 function App() {
   return (
-    <Router>
-      <div>
-        <nav>
-          <Nav>
-            <NavItem>
-              <Link to="/">Home</Link>
-            </NavItem>
-            <NavItem>
-              <Link to="/runs">Runs</Link>
-            </NavItem>
-          </Nav>
-        </nav>
+    <Container>
+      <Router>
+        <div>
+          <nav>
+            <Nav>
+              <NavItem>
+                <Link to="/">Home</Link>
+              </NavItem>
+              <NavItem>
+                <Link to="/runs">Runs</Link>
+              </NavItem>
+            </Nav>
+          </nav>
 
-        <Switch>
-          <Route path="/runs">
-            <Runs />
-          </Route>
-          <Route path="/">
-            <Home />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
+          <Switch>
+            <Route path="/runs">
+              <Runs />
+            </Route>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+    </Container>
   )
 }
 
