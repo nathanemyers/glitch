4cdc448edd997bc5bb2afc14eebb3c4be9351773
Nathan Myers <nathanemyers@mgmail.com>
2020-05-03 19:16:42 -0500

    stripping graphQL WAY back

--- graphql/resolvers/index.js
+++ graphql/resolvers/index.js
@@ -1,7 +1,4 @@
 import Image from './types/Image'
-import Run from './types/Run'
-import GlitchPattern from './types/GlitchPattern'
-import Instruction from './types/Instruction'
 
 import Query from './queries'
 import Mutation from './mutations'
